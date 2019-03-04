a5f66259c826706001fc355338623177487e7e8e
Jim Hunter <jim.hunter@goldenhippo.com>
2019-03-04 11:36:28 -0800

    Added emulations

--- wptests/conftest.py
+++ wptests/conftest.py
@@ -1,13 +1,17 @@
 from pytest import fixture
 from selenium import webdriver
-#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from .config import Config
 
 
 def pytest_addoption(parser):
     parser.addoption("--env",action="store", help="Environment to run against"),
+    parser.addoption("--emulation", action="store", help="mobile or desktop?"),
     parser.addoption("--browserlocation", action="store", help="Local or Browserstack")
+
+@fixture(scope="session")
+def emulation(request):
+    return request.config.getoption("--emulation")
 
 @fixture(scope="session")
 def browserlocation(request):
@@ -23,10 +27,13 @@ def app_config(env):
     return cfg
 
 @fixture(scope="function")
-def browser(browserlocation):  # Had: browserlocation
+def browser(browserlocation, emulation):  # Had: browserlocation
     # function will give you a browser for each test.
     # session will give you one browser for all tests.
     # Settings for Browserstack. Use tool to change: https://www.browserstack.com/automate/python
+    mobile_emulation = {"deviceName": "iPhone 6 Plus"}
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
     desired_cap = {
         'browser': 'Chrome',
         'browser_version': '62.0',
@@ -38,9 +45,20 @@ def browser(browserlocation):  # Had: browserlocation
         browser = webdriver.Remote(
             command_executor='http://developer372:C1EzDHLAwmW3chVsYZq7@hub.browserstack.com:80/wd/hub',
             desired_capabilities=desired_cap)
+    if emulation == 'mobile':
+        browser = webdriver.Chrome(chrome_options=chrome_options)
     else:
         browser = webdriver.Chrome()
     yield browser
     # Teardown
-    #browser.close()
+
+    #app_config.close()
+    #env.close()
     browser.quit()  # Required by browserstack to close, otherwise a timeout occurs.
+    try:
+        browser.close()
+    except:
+        this = 'that' # Getting resource warnings here when I don't use this try / except here. Odd... just using quit() or close(), or both close() and quit() together in either combination give the resource error.
+
+
+
